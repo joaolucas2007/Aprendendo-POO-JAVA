655a98ed80c989bc15213ef51dbbe6193d1f9a74
Joaolucas2007 <joao.git2000p@gmail.com>
2026-09-24 11:19:13 -0300

    Feat: Criando metodos em conta, e testando funcionamento

--- OrientacoesAObjeto/Conta.java
+++ OrientacoesAObjeto/Conta.java
@@ -4,4 +4,15 @@ public class Conta {
     double saldo;
     int numero;
     Agencia agencia; // A classe Conta possui uma agencia associada a ela
+
+    //Criando os metodos de Conta
+
+    //Metodo para inserir um valor
+    void deposita(double valor) {
+    saldo +=valor;}
+
+    //metodo para retirar um valor
+    void saca(double valor) {
+        saldo -= valor;
+    }
 }
---
+++ OrientacoesAObjeto/TestaConta.java
@@ -0,0 +1,21 @@
+package OrientacoesAObjeto;
+
+
+public class TestaConta {
+    public static void main(String[] args) {
+        Conta conta01 = new Conta();
+        //Adicionando valores ao objeto conta01
+        conta01.numero = 10;
+        conta01.saldo = 400;
+
+        //Testando metodo de depositar na conta01
+        conta01.deposita(500);
+
+
+        //testando metodo de sacar dinheiro na cont01
+        conta01.saca(200);
+
+        //Printando o saldo na conta após depositar e sacar dinheiro
+        System.out.println(conta01.saldo);
+    }
+}
