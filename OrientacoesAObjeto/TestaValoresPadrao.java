3f7d4d112580d9f3e2b781dfed62678580462d86
Joaolucas2007 <joao.git2000p@gmail.com>
2026-09-24 11:03:02 -0300

    Fix: Add valores a conta01, e criando agencia atraves de conta

--- OrientacoesAObjeto/TestaValoresPadrao.java
+++ OrientacoesAObjeto/TestaValoresPadrao.java
@@ -4,9 +4,23 @@ public class TestaValoresPadrao {
     public static void main(String[] args) {
         //Criando o objeto conta para testar os valores padrões de Conta
         Conta conta01 = new Conta();
-        //Printando os Valores padrões de conta
+        //Adicionando valores em conta01
+        conta01.saldo = 500.50;
+        conta01.numero = 123;
+
+        //Criando um Objeto Agencia Atraves de Conta
+
+        conta01.agencia = new Agencia();
+
+        //Adicioando valores aos atributos de Agencia atraves de conta
+        conta01.agencia.numero = 10; 
+        conta01.agencia.nome = "Agencia Central";
+
+
+        //Printando os Valores de conta e Agencia
         System.out.println(conta01.saldo);
         System.out.println(conta01.numero);
-        System.out.print(conta01.agencia);
+        System.out.println(conta01.agencia.nome);
+        System.out.println(conta01.agencia.numero);  
     }
 }
